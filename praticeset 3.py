218caaac37288b8f70617f23ebe9b6ae872c70e4
Mayank Charde <mayankcharde90@gmail.com>
2024-10-07 23:27:40 +0530

    Create praticeset 3.py

---
+++ praticeset 3.py
@@ -0,0 +1,77 @@
+# #  que 1 
+# words = {
+#     "madad": "Help",
+#     "kursi": "Chair",
+#     "billi": "Cat"
+# }
+# word = input("Enter the word you want meaning of: ")
+
+# print(words[word])
+
+
+#  que 2 
+# s =set()
+# n =int(input("enter number:"))
+# s.add(n)
+# n =int(input("enter number:"))
+# s.add(n)
+# n =int(input("enter number:"))
+# s.add(n)
+# n =int(input("enter number:"))
+# s.add(n)
+# n =int(input("enter number:"))
+# s.add(n)
+# n =int(input("enter number:"))
+# s.add(n)
+# n =int(input("enter number:"))
+# s.add(n)
+# n =int(input("enter number:"))
+# s.add(n)
+# n =int(input("enter number:"))
+# s.add(n)
+# print(s)
+
+#  que 3 
+s= set()
+s.add(18)
+s.add("18")
+print(s)
+
+#  que 4 
+s =set()
+s.add(20)
+s.add(20.0)
+s.add('20')
+print(s)
+print(len(s))
+
+#  que 5
+s ={}
+print(type(s))  # output dict
+
+#  que 6 
+# d = {}
+
+# name = input("Enter friends name: ")
+# lang = input("Enter Language name: ")
+# d.update({name: lang})
+
+# name = input("Enter friends name: ")
+# lang = input("Enter Language name: ")
+# d.update({name: lang})
+
+# name = input("Enter friends name: ")
+# lang = input("Enter Language name: ")
+# d.update({name: lang})
+
+# name = input("Enter friends name: ")
+# lang = input("Enter Language name: ")
+# d.update({name: lang})
+
+
+# print(d)
+
+
+#  que 7
+s ={3,444,55,"mayank",[2,3]}
+s[4][0] =9 # unhashable lst 
