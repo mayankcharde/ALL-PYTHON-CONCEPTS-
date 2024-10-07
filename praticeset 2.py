ddc8596e98e8d3b60272b06122faaf489c1d2152
Mayank Charde <mayankcharde90@gmail.com>
2024-10-07 23:27:13 +0530

    Create praticeset 2.py

---
+++ praticeset 2.py
@@ -0,0 +1,45 @@
+# que 1 
+# fruits =[]
+# f1 = input("enter fruits name:")
+# fruits.append(f1)
+# f2 = input("enter fruits name:")
+# fruits.append(f2)
+# f3 = input("enter fruits name:")
+# fruits.append(f3)
+# f4 = input("enter fruits name:")
+# fruits.append(f4)
+# f5 = input("enter fruits name:")
+# fruits.append(f5)
+# print(fruits)
+
+
+#  que 2 
+# marks = []
+
+# f1 = int(input("Enter Marks here: "))
+# marks.append(f1)
+# f2 = int(input("Enter Marks here: "))
+# marks.append(f2)
+# f3 = int(input("Enter Marks here: "))
+# marks.append(f3)
+# f4 = int(input("Enter Marks here: "))
+# marks.append(f4)
+# f5 = int(input("Enter Marks here: "))
+# marks.append(f5)
+# f6 = int(input("Enter Marks here: "))
+# marks.append(f6)
+# marks.sort()
+# print(marks)
+
+#  que 3
+# a = (34,45,"mayank")
+# a[2] = "tayank"
+
+# que 4 
+l = [3, 3, 5, 1]
+print(sum(l))
+
+# que 5 
+a = (7, 0, 8, 0, 0, 9)
+n = a.count(0)
+print(n)
